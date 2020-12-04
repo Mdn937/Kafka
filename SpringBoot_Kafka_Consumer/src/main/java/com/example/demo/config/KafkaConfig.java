d53c70ea51e2e804bab1e48d7ed7ee1fd8cccf18
Mdn937 <Mdn937@gmail.com>
2020-12-04 12:40:39 +0530

    first commit

---
+++ SpringBoot_Kafka_Consumer/src/main/java/com/example/demo/config/KafkaConfig.java
@@ -0,0 +1,72 @@
+package com.example.demo.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.example.demo.pojo.Friend;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+	
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory(){
+		Map<String, Object> configs = new HashMap();
+		configs.put( ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG , "localhost:9092");
+		configs.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+		configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		
+		
+		
+		return new DefaultKafkaConsumerFactory(configs);
+	}
+	
+	
+	@Bean 
+	public ConcurrentKafkaListenerContainerFactory kafkaListenerContainerFactory(){
+		ConcurrentKafkaListenerContainerFactory factory = new ConcurrentKafkaListenerContainerFactory();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
+	
+	
+	@Bean
+	public ConsumerFactory<String, Friend> consumerFriendFactory(){
+		Map<String, Object> configs = new HashMap();
+		configs.put( ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG , "localhost:9092");
+		configs.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
+		configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		
+		
+		
+		return new DefaultKafkaConsumerFactory<String, Friend>
+										(configs, 
+										new StringDeserializer(), 
+										new JsonDeserializer(Friend.class));
+				
+	}
+	
+
+	@Bean 
+	public ConcurrentKafkaListenerContainerFactory kafkaListenerFriendContainerFactory(){
+		ConcurrentKafkaListenerContainerFactory factory = new ConcurrentKafkaListenerContainerFactory();
+		factory.setConsumerFactory(consumerFriendFactory());
+		return factory;
+	}
+	
+	
+	
+
+}
---
+++ SpringBoot_Kafka_Producer/src/main/java/com/example/demo/restcontroller/KafkaRestController.java
@@ -0,0 +1,29 @@
+package com.example.demo.restcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.pojo.Friend;
+
+@RestController
+@RequestMapping("kafka")
+public class KafkaRestController {
+	
+	@Autowired
+	KafkaTemplate<String, Friend> kafkaTemplate;
+	
+	private static final String TOPIC = "topic1";
+
+	@GetMapping("/publish/friend")
+	public String publishFriendToKafka() {
+		
+		kafkaTemplate.send(TOPIC, new Friend("OBB", "Chennai"));
+		
+		return "Publish Friend is success!!!!";
+	}
+	
+}
